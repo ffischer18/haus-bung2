814a341ac0b42c8303986415e47c7aabc654e848
ffischer18 <Fischer.Fabian@sus.htl-grieskirchen.at>
2020-09-29 11:55:08 +0200

    HalloJavamitForEach

---
+++ Hausübung2/src/Hausübung2/Beispiel2/HalloJavamitForEach.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hausübung2.Beispiel2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ *
+ * @author fisch
+ */
+public class HalloJavamitForEach {
+    
+    public static void main(String[] args){
+        List<String> list = new ArrayList<>();
+        
+        for (String string : list) {
+            System.out.println(string);
+        }
+        
+        list.forEach((String s) -> System.out.println(s));
+        Consumer<String> consumer = (String s) -> System.out.println(s);
+        list.forEach(System.out :: print);
+    }
+    
+}
---
+++ Hausübung2/src/Hausübung2/Beispiel3/NumberTest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hausübung2.Beispiel3;
+
+/**
+ *
+ * @author fisch
+ */
+public interface NumberTest {
+    boolean testNumber(int number);
+}
