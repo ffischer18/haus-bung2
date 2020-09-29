349dc3d9d95009b9a9687ebd8df798ac27dff449
ffischer18 <Fischer.Fabian@sus.htl-grieskirchen.at>
2020-09-29 12:14:20 +0200

    NumberTest

--- Hausübung2/src/Hausübung2/Beispiel3/NumberTester.java
+++ Hausübung2/src/Hausübung2/Beispiel3/NumberTester.java
@@ -10,5 +10,29 @@ package Hausübung2.Beispiel3;
  * @author fisch
  */
 public class NumberTester {
-    
+
+    private final String fileName;
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile() {
+        
+    }
 }
