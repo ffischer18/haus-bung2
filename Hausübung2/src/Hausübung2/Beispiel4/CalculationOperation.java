aef4c4e66746e252fe2eec5e814adc474dcf30f9
ffischer18 <Fischer.Fabian@sus.htl-grieskirchen.at>
2020-09-29 12:57:14 +0200

    CalculationOperation

---
+++ Hausübung2/src/Hausübung2/Beispiel4/CalculationOperation.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hausübung2.Beispiel4;
+
+/**
+ *
+ * @author fisch
+ */
+public interface CalculationOperation {
+    Number calc(Number x, Number y);
+}
